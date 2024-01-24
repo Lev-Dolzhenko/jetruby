225ec5e37a06cdb58916f214b5481043e11e2cf2
Leo <workspace2707@yandex.ru>
2024-01-24 16:24:40 +0300

    created process block and capabilities

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
+/* INIT SWIPER */
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 125,
+  loop: true,
+  navigation: {
+    nextEl: ".wrapper__button_next",
+    prevEl: ".wrapper__button_prev",
+  },
+});
+
 /* INIT AOS */
 
 AOS.init({
