11c410c849e7de5e25ac3a5ab987b5f11d3d2f41
Leo <workspace2707@yandex.ru>
2024-01-25 16:31:58 +0300

    created adaptive

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,17 @@
+/* BURGER MENU */
+
+const burgerButton = document.getElementById("burgerButton");
+const navMobile = document.getElementById("navMobile");
+
+burgerButton.addEventListener("click", function () {
+  navMobile.classList.toggle("open");
+  navMobile.classList.toggle("none");
+  document.querySelector(".overlay").classList.toggle("open");
+});
+
+/* MODALS */
+
+
 /* INIT SWIPER */
 
 var swiper = new Swiper(".mySwiper", {
@@ -7,6 +21,18 @@ var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".wrapper__button_next",
     prevEl: ".wrapper__button_prev",
+  },
+  breakpoints: {
+    // when window width is <= 499px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30, // Правильное свойство для промежутка между слайдами
+    },
+    // when window width is <= 999px
+    1100: {
+      slidesPerView: 2,
+      spaceBetween: 40, // Правильное свойство для промежутка между слайдами
+    },
   },
 });
 
